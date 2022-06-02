de0b86486031568360bcdc43380f54573d57ffbd
Enzo Burille <95263677+eburille@users.noreply.github.com>
2022-06-02 01:54:25 -0300

    Update precoXmm.py
    Update to adapt de code for the data frame downloaded by downloader

--- precoXmm.py
+++ precoXmm.py
@@ -1,12 +1,4 @@
-import csv
-
-def mm_media_movel(cotações, period):
-    soma = 0
-    for cotação in cotações:
-        soma += cotação
-    media = soma / period
-
-    return media
+import pandas_datareader.data as web
 
 def mm_stats(cotação, media):
     if cotação > media:
@@ -32,9 +24,6 @@ def mm_rent(entrada, saida, status, patrimonio):
     else:
         return patrimonio
 
-def mm_ticker_adj(ticker):
-    return ticker.upper() + '.SA.csv'
-
 def mm_media_ganhos(retorno_ganho, med_ganho):
     return (retorno_ganho + med_ganho)/2
 
@@ -53,79 +42,83 @@ class Retorno():
     numero_operacoes = 0
     status_atual=''
     taxa_acerto = 0
-    longonly = 0
     first_data = 0
+    longonly = 0
+    
 
     def __init__(self, period):
         self.periodo = period
 
-def mm_main(period, ticker):
-    ticker_ajustado = mm_ticker_adj(ticker)
+class MM():
+    cotacoes = []
+    primeira_cot = 0
+
+    entrada = 0
+    saida = 0
+
+    def atual(self):
+        def media(cotacoes):
+            soma = 0
+            for cotacao in cotacoes:
+                soma += cotacao     
+            try: media = soma / len(cotacoes)
+            except: media = 0
+            return media
+
+        self.media = media(self.cotacoes)
+
+def mm_main(period, cot):
 
     retorno = Retorno(period)
-    ultima_cotacao = 0
+    mm = MM()
 
-    try:
-        f = open(ticker_ajustado)
-        f.close()
-    except: 
-        return 'erro'
+    mm.cotacoes = []
+
+    status = ''
+    check_status = ''
+
+    primeira_operação = True
         
-    with open(ticker_ajustado) as ativo:
-        read = csv.DictReader(ativo)
-        cotações_periodo = []
+    for indice, row in cot.iterrows():
 
-        media = 0
+        if len(mm.cotacoes) < retorno.periodo:
+            mm.cotacoes.append(float(row['Close']))
+            retorno.first_data = str(indice)[:10]
+            mm.primeira_cot = float(row['Adj Close'])
 
-        status = ''
-        check_status = ''
+        else:
+            mm.atual()
 
-        entrada = 0
-        saida = 0
+            check_status = status
+            status = mm_stats(float(row['Close']), mm.media)
 
-        primeira_operação = True
-            
-        for num, row in enumerate(read):
-            if len(cotações_periodo) < retorno.periodo:
-                cotações_periodo.append(float(row['Close']))
-                if num == 0:
-                    retorno.longonly = float(row['Open'])
-                    retorno.first_data = row['Date']
-
-            else:
-                cotações_periodo.pop(0)
-                cotações_periodo.append(float(row['Close']))
-                media = mm_media_movel(cotações_periodo, period)
-
-                check_status = status
-                status = mm_stats(float(row['Close']), media)
-
-                primeira_operação = mm_start(primeira_operação, status, check_status)
+            primeira_operação = mm_start(primeira_operação, status, check_status)
                 
-
-                ultima_cotacao = float(row['Close'])
-
-                if primeira_operação == True and check_status != status:
-                    entrada = saida
-                    saida = float(row['Close'])
-                    retorno.numero_operacoes += 1
-                    rentab = mm_rent(entrada, saida, status, retorno.patrimonio)
-                    retorno.patrimonio = rentab * retorno.patrimonio
-                    retorno.status_atual = status
-
-                    #print(row['Date'], status, retorno.patrimonio, row['Close'])
-
-                    if rentab > 1:
-                        retorno.operacoes_ganhas += 1
-                        retorno.media_ganhos = mm_media_ganhos(rentab, retorno.media_ganhos)
-                        if rentab > retorno.maior_retorno:
-                            retorno.maior_retorno = rentab
+            ultima_cotacao = float(row['Adj Close'])
+                
+            if primeira_operação == True and check_status != status:
                     
-                    elif rentab <= 1:
-                        retorno.operacoes_perdas += 1
-                        retorno.media_perdas = mm_media_perdas(rentab, retorno.media_perdas)
-                        if rentab < retorno.drawdown:
-                            retorno.drawdown = rentab
+                mm.entrada = mm.saida
+                mm.saida = float(row['Close'])
+                retorno.numero_operacoes += 1
+                rentab = mm_rent(mm.entrada, mm.saida, status, retorno.patrimonio)
+                retorno.patrimonio = rentab * retorno.patrimonio                    
+                retorno.status_atual = status
+
+                if rentab > 1:
+                    retorno.operacoes_ganhas += 1
+                    retorno.media_ganhos = mm_media_ganhos(rentab, retorno.media_ganhos)
+                    if rentab > retorno.maior_retorno:
+                        retorno.maior_retorno = rentab
+                    
+                elif rentab <= 1:
+                    retorno.operacoes_perdas += 1
+                    retorno.media_perdas = mm_media_perdas(rentab, retorno.media_perdas)
+                    if rentab < retorno.drawdown:
+                        retorno.drawdown = rentab
+                    
+            mm.cotacoes.pop(0)
+            mm.cotacoes.append(float(row['Close']))
 
 
     retorno.media_perdas = round(      (1-retorno.media_perdas)*100,       2)
@@ -136,7 +129,10 @@ def mm_main(period, ticker):
     retorno.retorno_total = round ((retorno.patrimonio - 1) * 100, 2)
     retorno.status_atual = status
     retorno.taxa_acerto = round(   retorno.operacoes_ganhas/retorno.numero_operacoes *100   , 2)
-    retorno.longonly = round((ultima_cotacao-retorno.longonly)/retorno.longonly*100, 2)
+    try:
+        retorno.longonly = round(    (ultima_cotacao - mm.primeira_cot) / mm.primeira_cot * 100  ,2)
+    except:
+        retorno.longonly = 0
 
     return retorno
 
